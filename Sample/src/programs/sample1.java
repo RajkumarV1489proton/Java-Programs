853ad6ab071468a83769f00d112fdd584577f264
RajkumarV1489 <RajkumarV1489@gmail.com>
2026-02-26 18:56:48 +0530

    Initial clean commit

---
+++ Sample/src/programs/sample1.java
@@ -0,0 +1,18 @@
+package programs;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+	
+
+			String s = "I love India";
+			
+			String c = s.substring(2);
+			
+			System.out.println(c);
+			} 
+
+
+	
+}
+
