ca275453f08013537c046a2315bb3b86da85c028
RajkumarV1489 <RajkumarV1489@gmail.com>
2026-02-27 00:03:30 +0530

    Modified sample1

---
+++ Sample/src/programs/Occurance_AAABBC_3A2B1C.java
@@ -0,0 +1,43 @@
+package programs;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Occurance_AAABBC_3A2B1C {
+
+	public static void main(String[] args) {
+		
+		String name = "AAABBC";
+		
+		LinkedHashMap<Character, Integer> mp = new LinkedHashMap<>();
+		
+		char S1 [] = name.toCharArray();
+		
+		for(Character ch : S1) {
+			
+			if(mp.containsKey(ch)) {
+				
+				Integer i = mp.get(ch);
+				
+				mp.put(ch, i+1);
+			} else {
+				
+				mp.put(ch, 1);
+			}
+			
+			
+			
+			
+		}
+		
+		for(Map.Entry<Character, Integer> entry : mp.entrySet()) {
+		
+		
+		System.out.print(entry.getKey()+""+entry.getValue());
+		
+		}
+	}
+
+}
