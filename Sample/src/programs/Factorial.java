c580c0ac28f5b5a792832ccbfcc2271068c845bc
RajkumarV1489 <RajkumarV1489@gmail.com>
2026-03-02 16:09:04 +0530

    Add Factorial Java program

---
+++ Sample/src/programs/Factorial.java
@@ -0,0 +1,25 @@
+package programs;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int fact =1;
+		int num =10;
+		
+		for(int i=1;i<num;i++ ) {
+			
+			fact = fact*i;
+			
+			
+		}
+		
+		System.out.println("Factorial of "+num+" is : "+fact);
+		
+		
+		
+		
+
+	}
+
+}
