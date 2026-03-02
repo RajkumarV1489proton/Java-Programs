add4258d99565264c5721f361288738a2d284827
raji <bharathytester@gmail.com>
2026-03-02 20:15:33 +0530

    Updated sample1 again

--- Sample/src/programs/sample1.java
+++ Sample/src/programs/sample1.java
@@ -5,11 +5,7 @@ public class sample1 {
 	public static void main(String[] args) {
 	
 
-<<<<<<< HEAD
-			String s = "I love Dubai";
-=======
 			String s = "I love Chennai";
->>>>>>> main
 			
 			String c = s.substring(2);
 			
