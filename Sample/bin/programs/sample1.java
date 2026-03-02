61bb060d60b498fdbabfb428af3e952cfafde93f
raji <bharathytester@gmail.com>
2026-03-02 19:36:31 +0530

    add deleted program

---
+++ Sample/bin/programs/sample1.java
@@ -0,0 +1,21 @@
+package programs;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+	
+
+			String s = "I love India";
+			
+			String c = s.substring(2);
+			
+			System.out.println(c);
+			
+			System.out.println(c);
+
+			} 
+
+
+	
+}
+
