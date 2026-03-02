33bd231220340bfa2c4bbb042029bcd7d838a474
raji <bharathytester@gmail.com>
2026-03-02 19:18:48 +0530

    Updated ReverseOfEachString

--- Sample/src/programs/ReverseOfEachString.java
+++ Sample/src/programs/ReverseOfEachString.java
@@ -2,23 +2,17 @@ package programs;
 
 public class ReverseOfEachString {
 
-	public static void main(String[] args) {
-	
-			 String str = "Welcome to java class";
-		        String[] words = str.split(" ");
-		        
-		        String rev = "";
-		        
-		        for(String word: words) {
-		        	
-		        	rev = word+" "+rev;
-	
-		        		}
-		        		
-		        		System.out.print(rev);
-	
-		        }
-			
-	
-		}
+    public static void main(String[] args) {
 
+        String str = "Welcome to java class";
+        String[] words = str.split(" ");
+
+        String rev = "";
+
+        for (String word : words) {
+            rev = word + " " + rev;
+        }
+
+        System.out.println(rev);
+    }
+}
